ce24182eefb31ec743f23a8dfd26930f41bbbdd3
iskero-nasoa <iskenderasanov7@icloud.com>
2026-06-21 20:20:28 +0600

    21/june/2026

--- leetcode.py
+++ leetcode.py
@@ -237,3 +237,18 @@ def is_anagram(f, s):
         else:
             second[i] = 1
     return first == second
+
+
+#21/june/2026
+def group_anagrams(words):
+    groups = {}
+
+    for word in words:
+        key = ''.join(sorted(word))
+
+        if key in groups:
+            groups[key].append(word)
+        else:
+            groups[key] = [word]
+
+    return list(groups.values())
