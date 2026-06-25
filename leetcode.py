fde6d03b0ea017d64a7916ba6db654fdfcfcf42c
iskero-nasoa <iskenderasanov7@icloud.com>
2026-06-25 14:16:16 +0600

    25/lune/2026

--- leetcode.py
+++ leetcode.py
@@ -307,3 +307,21 @@ def daily_temperatures(temperatures):
             answer.append(0)
 
     return answer
+
+
+#25/june/2026
+def max_area(height):
+    left = 0
+    right = len(height) - 1
+    best = 0
+    while left < right:
+        width = right - left
+        h = min(height[left], height[right])
+        area = width * h
+        if area > best:
+            best = area
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+    return best
