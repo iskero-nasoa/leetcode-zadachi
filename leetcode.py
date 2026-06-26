83afb02568e7eee617ae195172d360968505c77f
iskero-nasoa <iskenderasanov7@icloud.com>
2026-06-26 12:53:51 +0600

    26/june/2026

--- leetcode.py
+++ leetcode.py
@@ -325,3 +325,18 @@ def max_area(height):
         else:
             right -= 1
     return best
+
+
+#26/june/2026
+def binary_search(nums, target):
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
